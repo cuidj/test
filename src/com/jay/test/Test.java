a3d5e756e415e87872892ac476d056cf9e1e9336
cuidj <1315570800@qq.com>
2016-02-29 17:09:02 +0800

    【all】创建项目

---
+++ src/com/jay/test/Test.java
@@ -0,0 +1,17 @@
+package com.jay.test;
+
+import com.jay.pojo.Student;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s1 = new Student();	
+		System.out.println(s1.getIds());
+	}
+
+}
---
+++ src/com/jay/test/Test002.java
@@ -0,0 +1,19 @@
+package com.jay.test;
+
+import com.jay.pojo.Student;
+
+public class Test002 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student ss = new Student();
+		Student s1 = new Student();
+		System.out.println(ss.getIds());
+
+	}
+
+}
